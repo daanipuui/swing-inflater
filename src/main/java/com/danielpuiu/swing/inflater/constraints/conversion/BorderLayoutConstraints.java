317baaaa46517ae60c5c79914c87c4555a6d3366
Daniel PUIU <danielpuiu@windowslive.com>
2019-03-11 23:38:48 +0200

    fixed border layout constraint issue + test

--- src/main/java/com/danielpuiu/swing/inflater/constraints/conversion/BorderLayoutConstraints.java
+++ src/main/java/com/danielpuiu/swing/inflater/constraints/conversion/BorderLayoutConstraints.java
@@ -7,21 +7,10 @@ import java.awt.BorderLayout;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
-import static java.awt.BorderLayout.AFTER_LAST_LINE;
-import static java.awt.BorderLayout.AFTER_LINE_ENDS;
-import static java.awt.BorderLayout.BEFORE_FIRST_LINE;
-import static java.awt.BorderLayout.BEFORE_LINE_BEGINS;
 import static java.awt.BorderLayout.CENTER;
-import static java.awt.BorderLayout.EAST;
-import static java.awt.BorderLayout.NORTH;
-import static java.awt.BorderLayout.SOUTH;
-import static java.awt.BorderLayout.WEST;
 
 public class BorderLayoutConstraints implements ConstraintsConversion {
-
-    private static final List<String> POSITIONS = Arrays.asList(CENTER, EAST, WEST, SOUTH, NORTH, BEFORE_FIRST_LINE, AFTER_LAST_LINE, BEFORE_LINE_BEGINS, AFTER_LINE_ENDS);
 
     @Override
     public List<String> getHandledLayouts() {
@@ -31,11 +20,6 @@ public class BorderLayoutConstraints implements ConstraintsConversion {
     @Override
     public Object convert(ContextProvider contextProvider, Map<String, String> map) {
         String value = map.getOrDefault("position", CENTER);
-        String constraint = (String) convertConstant(contextProvider, "BorderLayout." + value);
-        if (Objects.isNull(constraint)) {
-            constraint = value;
-        }
-
-        return POSITIONS.contains(constraint) ? constraint : CENTER;
+        return convertConstant(contextProvider, "BorderLayout." + value);
     }
 }

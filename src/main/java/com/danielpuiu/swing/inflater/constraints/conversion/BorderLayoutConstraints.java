872f91c4674195245377b873b7d738bf5993fa98
Daniel PUIU <danielpuiu@windowslive.com>
2019-03-11 23:36:19 +0200

    fixed border layout constraint issue + test

--- src/main/java/com/danielpuiu/swing/inflater/constraints/conversion/BorderLayoutConstraints.java
+++ src/main/java/com/danielpuiu/swing/inflater/constraints/conversion/BorderLayoutConstraints.java
@@ -9,6 +9,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
+import static java.awt.BorderLayout.AFTER_LAST_LINE;
+import static java.awt.BorderLayout.AFTER_LINE_ENDS;
+import static java.awt.BorderLayout.BEFORE_FIRST_LINE;
+import static java.awt.BorderLayout.BEFORE_LINE_BEGINS;
 import static java.awt.BorderLayout.CENTER;
 import static java.awt.BorderLayout.EAST;
 import static java.awt.BorderLayout.NORTH;
@@ -17,7 +21,7 @@ import static java.awt.BorderLayout.WEST;
 
 public class BorderLayoutConstraints implements ConstraintsConversion {
 
-    private static final List<String> POSITIONS = Arrays.asList(CENTER, EAST, WEST, SOUTH, NORTH);
+    private static final List<String> POSITIONS = Arrays.asList(CENTER, EAST, WEST, SOUTH, NORTH, BEFORE_FIRST_LINE, AFTER_LAST_LINE, BEFORE_LINE_BEGINS, AFTER_LINE_ENDS);
 
     @Override
     public List<String> getHandledLayouts() {

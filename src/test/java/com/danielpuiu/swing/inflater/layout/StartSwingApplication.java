b325447d133ff4d42a1ea04bd6e34579af9477d4
Daniel PUIU <danielpuiu@windowslive.com>
2019-06-26 23:59:38 +0300

    Align test with readme file

--- src/test/java/com/danielpuiu/swing/inflater/layout/StartSwingApplication.java
+++ src/test/java/com/danielpuiu/swing/inflater/layout/StartSwingApplication.java
@@ -4,19 +4,21 @@ import com.danielpuiu.swing.inflater.xml.ComponentLoader;
 
 import javax.swing.*;
 
+import java.io.InputStream;
+
 import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class StartSwingApplication {
 
     public static void main(String[] args) {
-        String resourceName = "complex_layout.xml";
+        InputStream inputStream = StartSwingApplication.class.getClassLoader().getResourceAsStream("complex_layout.xml");
 
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame();
             frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
             ComponentLoader loader = new ComponentLoader();
-            JPanel container = loader.load(StartSwingApplication.class.getClassLoader().getResourceAsStream(resourceName));
+            JPanel container = loader.load(inputStream);
             frame.getContentPane().add(container);
 
             frame.pack();
